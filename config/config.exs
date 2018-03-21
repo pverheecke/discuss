f990a82a60630b97baab8427e9f690c70be6d3e5
Peter Verheecke <pverheecke@Peters-iMac.local>
2018-03-20 20:08:14 -0700

    Finish topic CRUD and start auth

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,12 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "0e7274a6f41392af7707",
+  client_secret: "7b64b98ade56df4587e24ce215ce613df7347b8e"
